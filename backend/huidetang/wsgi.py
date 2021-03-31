14cef50bedb5bca5fe5f99f11e461913e75df2c5
huideyeren <takakura.yusuke@gmail.com>
2021-03-31 21:33:09 +0900

    Setting default env to localhost.

--- backend/huidetang/wsgi.py
+++ backend/huidetang/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "huidetang.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "huidetang.settings.localhost")
 
 application = get_wsgi_application()
